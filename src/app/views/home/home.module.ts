633099a36b7f668b8b15399e930d0f05b206bc04
Yahir Martinez <yahirventas2023@gmail.com>
2023-07-14 09:23:56 -0600

    Registrar usuario y datos del usuario

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { UserDetailComponent } from './userDetail/user-detail/user-detail.component';
+import { UserInsertComponent } from './userDetail/user-insert/user-insert.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserDetailComponent,
+    UserInsertComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
---
+++ src/app/views/home/userDetail/user-insert/user-insert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-insert',
+  templateUrl: './user-insert.component.html',
+  styleUrls: ['./user-insert.component.scss']
+})
+export class UserInsertComponent {
+
+}
